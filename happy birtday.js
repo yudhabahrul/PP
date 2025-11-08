6b011030ec66d234e60de3e34be9c497ea11b396
yudhabahrul <74826706+yudhabahrul@users.noreply.github.com>
2025-11-08 16:06:52 +0700

    Update happy birtday.js

--- happy birtday.js
+++ happy birtday.js
@@ -5,7 +5,7 @@ let w = (c.width = window.innerWidth),
 (hh = h / 2),
   (opts = {
     // change the text in here //
-    strings: ["Maafkan saya", "UKHTI", "ZARE"],
+    strings: ["MAAFKAN", "SAYA UKHTI", "ZARE"],
     charSize: 30,
     charSpacing: 35,
     lineHeight: 40,
